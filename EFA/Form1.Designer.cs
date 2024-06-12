e9b0768650e057151d0aa4d3ec0533be2929ffe8
Jorge Calderon <jorgecalderon.jcrhh@gmail.com>
2024-06-12 13:38:22 -0400

    Agregar archivos de proyecto.

---
+++ EFA/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace EFA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panelColor = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(462, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Agregar imagen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(462, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cambiar imagen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(33, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(407, 334);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(621, 35);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(407, 334);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(343, 400);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(388, 192);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(446, 138);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // panelColor
+            // 
+            this.panelColor.Location = new System.Drawing.Point(475, 164);
+            this.panelColor.Name = "panelColor";
+            this.panelColor.Size = new System.Drawing.Size(89, 17);
+            this.panelColor.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1072, 643);
+            this.Controls.Add(this.panelColor);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panelColor;
+    }
+}
+
---
+++ EFA/Form1.cs
@@ -0,0 +1,208 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EFA
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<Color, Color> mapeoColores = new Dictionary<Color, Color>();
+        private Dictionary<Point, Color> coloresOriginales = new Dictionary<Point, Color>();
+        private int tolerancia = 30; // Definir un rango de tolerancia para los colores
+        private Bitmap imagenOriginal;
+        private Bitmap imagenModificada;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.MouseClick += PictureBox2_MouseClick;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            ConectarBaseDatos();
+            CargarColoresDesdeBD();
+            CargarDatosEnDataGridView();
+        }
+
+        private void ConectarBaseDatos()
+        {
+            string cadenaConexion = "server=localhost;database=bdcolores;uid=root;pwd=;";
+            MySqlConnection conexion = new MySqlConnection(cadenaConexion);
+            try
+            {
+                conexion.Open();
+                MessageBox.Show("¡Conexión Abierta!");
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("¡No se puede abrir la conexión! " + ex.Message);
+            }
+        }
+
+        private void CargarColoresDesdeBD()
+        {
+            string cadenaConexion = "server=localhost;database=bdcolores;uid=root;pwd=;";
+            MySqlConnection conexion = new MySqlConnection(cadenaConexion);
+            try
+            {
+                conexion.Open();
+                string consulta = "SELECT ColorOriginal, ColorModificado FROM ModificaColor";
+                MySqlCommand comando = new MySqlCommand(consulta, conexion);
+                MySqlDataReader lector = comando.ExecuteReader();
+                while (lector.Read())
+                {
+                    string colorOriginalString = lector["ColorOriginal"].ToString();
+                    string colorCambioString = lector["ColorModificado"].ToString();
+                    Color colorOriginal = ParsearColor(colorOriginalString);
+                    Color colorCambio = ParsearColor(colorCambioString);
+                    mapeoColores[colorOriginal] = colorCambio;
+                }
+                lector.Close();
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("¡Error al cargar colores desde la base de datos! " + ex.Message);
+            }
+        }
+
+        // Resto del código para cargar imágenes y aplicar cambios...
+    
+
+// Subir imagen y mostrar en PictureBox sin modificarla
+private void button1_Click(object sender, EventArgs e)
+            {
+                OpenFileDialog abrirDialogo = new OpenFileDialog();
+                abrirDialogo.Filter = "Archivos de Imagen(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+                if (abrirDialogo.ShowDialog() == DialogResult.OK)
+                {
+                    imagenOriginal = new Bitmap(abrirDialogo.FileName);
+                    pictureBox1.Image = imagenOriginal;
+                    coloresOriginales.Clear(); // Limpiar colores originales anteriores
+                }
+            }
+
+            // Aplicar cambios a la imagen basada en la base de datos y mostrar en PictureBox2
+            private void button2_Click(object sender, EventArgs e)
+            {
+                if (imagenOriginal == null)
+                {
+                    MessageBox.Show("Primero sube una imagen.");
+                    return;
+                }
+
+                imagenModificada = CambiarColoresSegunBD(new Bitmap(imagenOriginal));
+                pictureBox2.Image = imagenModificada;
+            }
+
+            // Función para cambiar píxeles basado en colores de la base de datos
+            private Bitmap CambiarColoresSegunBD(Bitmap bitmap)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    for (int x = 0; x < bitmap.Width; x++)
+                    {
+                        Color colorPixel = bitmap.GetPixel(x, y);
+                        foreach (var mapeo in mapeoColores)
+                        {
+                            if (EsColorEnRango(colorPixel, mapeo.Key))
+                            {
+                                coloresOriginales[new Point(x, y)] = colorPixel;
+                                CambiarPixelesCircundantes(bitmap, x, y, mapeo.Value);
+                                break;
+                            }
+                        }
+                    }
+                }
+                return bitmap;
+            }
+
+            private bool EsColorEnRango(Color color1, Color color2)
+            {
+                return Math.Abs(color1.R - color2.R) <= tolerancia &&
+                       Math.Abs(color1.G - color2.G) <= tolerancia &&
+                       Math.Abs(color1.B - color2.B) <= tolerancia;
+            }
+
+            private Color ParsearColor(string colorString)
+            {
+                string[] partes = colorString.Split(',');
+                if (partes.Length == 3)
+                {
+                    int r = int.Parse(partes[0]);
+                    int g = int.Parse(partes[1]);
+                    int b = int.Parse(partes[2]);
+                    return Color.FromArgb(r, g, b);
+                }
+                return Color.White;
+            }
+
+            private void CambiarPixelesCircundantes(Bitmap bitmap, int x, int y, Color nuevoColor)
+            {
+                int rango = 20;
+
+                for (int dy = -rango; dy <= rango; dy++)
+                {
+                    for (int dx = -rango; dx <= rango; dx++)
+                    {
+                        int nuevoX = x + dx;
+                        int nuevoY = y + dy;
+
+                        if (nuevoX >= 0 && nuevoX < bitmap.Width && nuevoY >= 0 && nuevoY < bitmap.Height)
+                        {
+                            Point point = new Point(nuevoX, nuevoY);
+                            if (!coloresOriginales.ContainsKey(point))
+                            {
+                                coloresOriginales[point] = bitmap.GetPixel(nuevoX, nuevoY);
+                            }
+                            bitmap.SetPixel(nuevoX, nuevoY, nuevoColor);
+                        }
+                    }
+                }
+            }
+
+        private void PictureBox2_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (imagenModificada == null) return;
+
+            // Obtener el punto de clic relativo al PictureBox2
+            Point punto = ObtenerCoordenadasImagen(e.Location, pictureBox2, imagenModificada);
+            if (punto.X < 0 || punto.Y < 0 || punto.X >= imagenModificada.Width || punto.Y >= imagenModificada.Height)  // Corregido: 'or' cambiado a '||'
+                return;
+
+            Color colorClickeado = imagenModificada.GetPixel(punto.X, punto.Y);
+            if (coloresOriginales.ContainsKey(punto))
+            {
+                Color colorOriginal = coloresOriginales[punto];
+                textBox1.Text = $"Original: {colorOriginal} (RGB: {colorOriginal.R}, {colorOriginal.G}, {colorOriginal.B})\n" +
+                                $"Cambiado: {colorClickeado} (RGB: {colorClickeado.R}, {colorClickeado.G}, {colorClickeado.B})";
+                panelColor.BackColor = colorClickeado;
+            }
+            else
+            {
+                textBox1.Text = $"Cambiado: {colorClickeado} (RGB: {colorClickeado.R}, {colorClickeado.G}, {colorClickeado.B})";
+                panelColor.BackColor = colorClickeado;
+            }
+        }
+
+
+        private Point ObtenerCoordenadasImagen(Point punto, PictureBox pictureBox, Bitmap imagen)
+            {
+                int x = punto.X * imagen.Width / pictureBox.Width;
+                int y = punto.Y * imagen.Height / pictureBox.Height;
+                return new Point(x, y);
+            }
+
+            private void pictureBox1_Click(object sender, EventArgs e)
+            {
+            }
+        }
+    }
+
